c87ea86c2f3019bae689c140779d912b42c748aa
naiker gomez <gomeznaiker@gmail.com>
2025-09-29 05:13:36 +0000

    mete ese archivo en un .evn en un ..gitignore

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -10,7 +10,7 @@ from app.database import prisma
 from app.schemas import User
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "tu_super_secreto_aqui"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
