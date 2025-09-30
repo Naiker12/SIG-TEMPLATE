e60e6aa5b1a990f01e1465dcf0126745601914b4
naiker12 <gomeznaiker@gmail.com>
2025-09-29 22:33:16 -0500

    Se mdofico el backend

--- backend/app/core/middleware.py
+++ backend/app/core/middleware.py
@@ -1,8 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
-    # Para desarrollo, permitir todos los orígenes es lo más simple.
-    # En producción, deberías restringirlo a la URL de tu frontend.
+
     origins = ["*"]
 
     app.add_middleware(
