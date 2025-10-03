dda614e07eb9fd1f317c0fd54ba23801336faff1
naiker gomez <gomeznaiker@gmail.com>
2025-10-03 22:10:43 +0000

    ahora es el sidebar Runtime Error
    Error: SidebarGroupContent is not de

--- frontend/src/components/dashboard/sidebar.tsx
+++ frontend/src/components/dashboard/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -29,6 +30,7 @@ import {
   SheetContent,
   SidebarGroup,
   SidebarGroupLabel,
+  SidebarGroupContent,
 } from "@/components/ui/sidebar";
 import {
   Collapsible,
