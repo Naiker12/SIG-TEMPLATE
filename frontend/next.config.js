bef65eac7be440659c6d2157708794c4655a2a47
naiker gomez <gomeznaiker@gmail.com>
2025-10-11 04:23:05 +0000

    analiza este errro que sale todavia y no lo ha socionado vale dame una s

---
+++ frontend/next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compress: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        port: '',
+        pathname: '/api/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+        '*.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev',
+    ],
+  },
+};
+
+module.exports = nextConfig;
