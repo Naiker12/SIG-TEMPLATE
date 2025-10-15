1e505222215c343c908b1847fe4f6091fce49f0d
naiker gomez <gomeznaiker@gmail.com>
2025-10-15 02:49:45 +0000

    pero sabe que yo puedo pornel la pai que quiera , y alguna api pide toke

--- frontend/src/app/extraer-apis/api-personalizada/page.tsx
+++ frontend/src/app/extraer-apis/api-personalizada/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { TopBar } from "@/components/dashboard/topbar";
 import { Button } from "@/components/ui/button";
